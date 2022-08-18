60a3c861d648a1a906867d75895399d5f01c4038
Sruthi <sruthigadhiaju@gmail.com>
2022-08-18 14:37:56 +0530

    transftering

---
+++ src/main/java/icicifunds/App.java
@@ -0,0 +1,13 @@
+package icicifunds;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
