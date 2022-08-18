d01f51091cfa337d142c2a9190bc7c00f388daf3
Sruthi <sruthigadhiaju@gmail.com>
2022-08-18 14:44:46 +0530

    login transfer

---
+++ src/main/java/iciclogin/App.java
@@ -0,0 +1,13 @@
+package iciclogin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
